a53665599f5f6541b8e3774f24b9e2c3ff245439
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-26 02:10:33 +0700

    Full Height Emerald BG

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="h-100">
         <router-view />
     </div>
 </template>
+<style src="./global.css" />
--- frontend/src/screens/Reservation/Reservation.vue
+++ frontend/src/screens/Reservation/Reservation.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid background h-100">
 
     </div>
 </template>
+<style scoped>
+.background {
+    background: var(--emerald);
+}
+</style>
