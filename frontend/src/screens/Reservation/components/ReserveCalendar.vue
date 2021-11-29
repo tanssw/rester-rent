83685f16128f027896c3fd9789950e588f272fd2
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-29 18:53:47 +0700

    Prepare for date selector

--- frontend/src/screens/Reservation/components/ReserveCalendar.vue
+++ frontend/src/screens/Reservation/components/ReserveCalendar.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
-        <calendar is-expanded class="custom-calendar">
+        <calendar is-expanded trim-weeks class="custom-calendar">
             <template v-slot:day-content="{day}">
                 <div>
-                    <div class="p-4 text-center">{{ day.day }}</div>
+                    <button @click="select(day.id)" class="date h5 fw-light mb-0 p-4 text-center w-100 border-0 bg-white">
+                        {{ day.day }}
+                    </button>
                 </div>
             </template>
         </calendar>
     </div>
 </template>
 <style scoped>
-.vc-header {
-    padding: 16px !important
+.date {
+    color: #333;
+    transition: .25s;
+}
+.date:hover {
+    color: var(--salmon);
 }
 </style>
 <script>
@@ -20,6 +26,12 @@ import { Calendar } from 'v-calendar'
 export default {
     components: {
         Calendar
+    },
+    methods: {
+        select(date) {
+            // TODO: Function when date selected
+            console.log(date)
+        }
     }
 }
 </script>
