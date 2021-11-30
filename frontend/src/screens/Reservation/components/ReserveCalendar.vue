4d7d5ed16cf7c0d83627e1ed2a900d7bbf45b77e
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-30 16:03:21 +0700

    Add dayjs lib

--- frontend/src/screens/Reservation/components/ReserveCalendar.vue
+++ frontend/src/screens/Reservation/components/ReserveCalendar.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
+        <div class="d-flex justify-content-between align-items-centerr mb-4">
+            <h3 class="mb-0">
+                <span class="text-secondary fw-light">Selected Date:&nbsp;</span>
+                <span class="text-emerald">{{selected}}</span>
+            </h3>
+            <button class="btn btn-primary">Select</button>
+        </div>
         <calendar is-expanded trim-weeks class="custom-calendar">
             <template v-slot:day-content="{day}">
                 <div>
                     <button @click="select(day.id)" class="date h5 fw-light mb-0 p-4 text-center w-100 border-0 bg-white">
-                        {{ day.day }}
+                        {{day.day}}
                     </button>
                 </div>
             </template>
@@ -19,18 +26,28 @@
 .date:hover {
     color: var(--salmon);
 }
+.btn-select {
+
+}
 </style>
 <script>
+import dayjs from 'dayjs'
+
 import { Calendar } from 'v-calendar'
 
 export default {
     components: {
         Calendar
     },
+    data() {
+        return {
+            selected: null
+        }
+    },
     methods: {
         select(date) {
             // TODO: Function when date selected
-            console.log(date)
+            this.selected = dayjs(date).format('DD MMMM YYYY')
         }
     }
 }
