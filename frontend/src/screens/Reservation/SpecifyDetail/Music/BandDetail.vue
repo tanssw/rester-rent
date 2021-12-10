3a47c8851a199b76346505634eae40d63284cfb2
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-12-10 18:38:44 +0700

    Format date

--- frontend/src/screens/Reservation/SpecifyDetail/Music/BandDetail.vue
+++ frontend/src/screens/Reservation/SpecifyDetail/Music/BandDetail.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="mt-4">
         <h4 class="fw-light">กรุณาเลือกวงดนตรี</h4>
-        <div>รายชื่อวงดนตรีที่สามารถขึ้นเล่นได้ในวันที่ {{date}}</div>
+        <div>รายชื่อวงดนตรีที่สามารถขึ้นเล่นได้ในวันที่ {{formattedDate}}</div>
+        <div class="mt-4">
+
+        </div>
     </div>
 </template>
 <script>
+import dayjs from 'dayjs'
 export default {
     props: {
         date: {
             type: String
+        }
+    },
+    computed: {
+        formattedDate() {
+            return this.date ? dayjs(this.date).format('DD MMM YYYY') : ''
         }
     },
     data() {
