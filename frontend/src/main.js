9fe2e9297998a73fd095b154926b49a4da78100a
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-12-15 20:55:49 +0700

    Initialize firebase project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,11 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './config/firebaseConfig'
+
+initializeApp(firebaseConfig)
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
---
+++ frontend/src/screens/Admin/SignIn/SignIn.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container-fluid background py-5">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-6 offset-3">
+                    <div class="bg-white rounded-3 p-5">
+                        <h3>
+                            <span class="fw-bold text-emerald">Rester Rent</span>
+                            <span class="fw-lighter text-secondary"> | Management</span>
+                        </h3>
+                        <div class="mt-4">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.background {
+    background-color: var(--emerald);
+    min-height: 100vh;
+}
+</style>
+<script>
+export default {
+
+}
+</script>
