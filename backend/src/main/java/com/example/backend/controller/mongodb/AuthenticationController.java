24a038783adb3b1616034a873586f40e413630ac
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-12-16 09:30:02 +0700

    Setup for different env

--- backend/src/main/java/com/example/backend/controller/mongodb/AuthenticationController.java
+++ backend/src/main/java/com/example/backend/controller/mongodb/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.example.backend.controller.mongodb;
 
+import com.example.backend.requestBody.AuthData;
 import com.example.backend.service.mongodb.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,8 @@ public class AuthenticationController {
     private AuthenticationService authenticationService;
 
     @RequestMapping(value="/auth", method = RequestMethod.POST)
-    public void authenication() {
-
+    public void authenication(@RequestBody AuthData authData) {
+        System.out.println(authData.toString());
     }
 
     @RequestMapping(value="/token", method = RequestMethod.POST)
