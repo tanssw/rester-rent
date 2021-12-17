3fe55d47885fa8a51fcb4c27fc5360a7c6d04601
l3luescreen <hacky404@hotmail.com>
2021-12-17 17:31:11 +0700

    can't delete if id is not exist

--- backend/src/main/java/com/example/backend/repository/mysql/ThemeAccessoryRepository.java
+++ backend/src/main/java/com/example/backend/repository/mysql/ThemeAccessoryRepository.java
@@ -11,6 +11,11 @@ import javax.transaction.Transactional;
 @Repository
 public interface ThemeAccessoryRepository extends JpaRepository<ThemeAccessory, String> {
 
+//    @Transactional
+//    @Modifying
+//    @Query(value = "UPDATE THEME_ACCESSORY SET ACCESSORY_ID=?1 WHERE THEME_ACCESSORY.THEME_ID=?2", nativeQuery = true)
+//    void updateTA(int accessoryId, int themeId);
+
     @Transactional
     @Modifying
     @Query(value = "DELETE FROM THEME_ACCESSORY WHERE THEME_ID=?1 AND ACCESSORY_ID=?2", nativeQuery = true)
