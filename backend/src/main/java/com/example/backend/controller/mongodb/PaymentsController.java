0ab5fb5651dac9a2d8cb5a3b812a941b1a892287
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-12-18 00:48:50 +0700

    Ignore mail failure

--- backend/src/main/java/com/example/backend/controller/mongodb/PaymentsController.java
+++ backend/src/main/java/com/example/backend/controller/mongodb/PaymentsController.java
@@ -2,6 +2,7 @@ package com.example.backend.controller.mongodb;
 
 import com.example.backend.pojo.mongodb.Payments;
 import com.example.backend.requestBody.AuthTokenData;
+import com.example.backend.service.mongodb.MailService;
 import com.example.backend.service.mongodb.PaymentsService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class PaymentsController {
 
     @Autowired
     private PaymentsService paymentsService;
+
+    @Autowired
+    private MailService mailService;
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
@@ -33,10 +37,17 @@ public class PaymentsController {
         AuthTokenData header = new AuthTokenData(token, userId);
         Object auth = rabbitTemplate.convertSendAndReceive("AuthExchange", "auth", header);
         if ((boolean) auth) {
-            if (paymentsService.addOrUpdatePayment(payments)) {
+            try {
+                if (!paymentsService.addOrUpdatePayment(payments)) {
+                    throw new Exception();
+                }
+                if (payments.isStatus()) {
+                    mailService.sendMail(payments, true);
+                }
                 return new ResponseEntity("Update payment successfully.", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            return new ResponseEntity("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
