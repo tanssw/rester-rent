e34b062c430c30c8b6ee221503897e82ee6ceb4e
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-12-17 19:43:22 +0700

    Mixin method for checking 401

--- frontend/src/mixin.js
+++ frontend/src/mixin.js
@@ -3,6 +3,11 @@ const mixin = {
         destroyToken() {
             localStorage.removeItem('RR-Token')
             localStorage.removeItem('RR-UID')
+        },
+        checkUnauthorized(error) {
+            const status = error.response.status
+            if (status !== 401) return
+            this.destroyToken()
         }
     }
 }
